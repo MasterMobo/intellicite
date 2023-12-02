e4e09114789eb0da5847855e7d10054e72e2c705
Duong Tran Le Truong <truongduongtranle@gmail.com>
2023-12-02 23:33:19 +0700

    add keyword input prompt and search similar words above a certain threshold
    now to find how to unzip the damn thing for speeding up stuffs
    
    there's a pretrained text vector model I downloaded at https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/edit?resourcekey=0-wjGZdNAUop6WykTtMip30g and put in the same directory as relevant text detect.py

--- text-highlighting/relevant-text-detect.py
+++ text-highlighting/relevant-text-detect.py
@@ -1,24 +1,45 @@
-# Import the NLTK library
+# Import the NLTK and gensim libraries
 import nltk
+import gensim
+
+filename='GoogleNews-vectors-negative300.bin.gz'
+
+# Load a pre-trained word2vec model
+model = gensim.models.KeyedVectors.load_word2vec_format(filename, binary=True)
 
 # Define a paragraph of text
 paragraph = "Natural language processing (NLP) is a specialized field for analysis and generation of human languages. Human languages, rightly called natural language, are highly context-sensitive and often ambiguous in order to produce a distinct meaning. (Remember the joke where the wife asks the husband to \"get a carton of milk and if they have eggs, get six,\" so he gets six cartons of milk because they had eggs.) NLP provides the ability to comprehend natural language input and produce natural language output appropriately."
 
-# Define a list of keywords
-keywords = ["natural language"]
+# Ask the user to enter a list of keywords separated by commas
+print("Please enter a list of keywords separated by commas:")
+# Read the user input and split it by commas
+keywords = input().split(",")
+
+# Define a similarity threshold
+threshold = 0.8
 
 # Tokenize the paragraph into sentences
 sentences = nltk.sent_tokenize(paragraph)
 
-# Loop through the sentences and check if they contain any of the keywords
+# Loop through the sentences and check if they contain any of the keywords or similar words
 for sentence in sentences:
     # Convert the sentence to lower case
     sentence = sentence.lower()
+    # Tokenize the sentence into words
+    words = nltk.word_tokenize(sentence)
     # Loop through the keywords
     for keyword in keywords:
-        # Check if the keyword is in the sentence
-        if keyword in sentence:
-            # Print the sentence
-            print(sentence)
-            # Break the inner loop
-            break
+        # Strip any whitespace from the keyword
+        keyword = keyword.strip()
+        # Loop through the words in the sentence
+        for word in words:
+            # Check if the word and the keyword are in the model's vocabulary
+            if word in model and keyword in model:
+                # Calculate the cosine similarity between the word and the keyword
+                similarity = model.similarity(word, keyword)
+                # Check if the similarity is above the threshold
+                if similarity > threshold:
+                    # Print the sentence
+                    print(sentence)
+                    # Break the inner loops
+                    break
