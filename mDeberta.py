077ed4a680dae5d56c5c94162f31f922ec0a067c
Alakinnn <117524098+Alakinnn@users.noreply.github.com>
2023-12-03 22:35:59 +0700

    mdeberta model uploaded
    This shit is fucking superior, only problem is gpu

---
+++ mDeberta.py
@@ -0,0 +1,17 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+model_name = "MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+premise = "The constant demand for productivity has made people more industrious, driving them to enhance time-management skills and adopt innovative technologies."
+hypothesis = "The people of the 21st century are hardworking due to the competitive environment, where success often hinges on one's ability to navigate challenges, adapt to rapid changes, and consistently strive for excellence. The relentless pursuit of goals is fueled by a globalized world that demands continuous innovation and a strong work ethic. In this era, individuals are motivated not only by personal ambition but also by the necessity to stay relevant and contribute meaningfully in a dynamic and ever-evolving professional landscape."
+
+input = tokenizer(premise, hypothesis, truncation=True, return_tensors="pt")
+output = model(input["input_ids"].to(device))  # device = "cuda:0" or "cpu"
+prediction = torch.softmax(output["logits"][0], -1).tolist()
+label_names = ["entailment", "neutral", "contradiction"]
+prediction = {name: round(float(pred) * 100, 1) for pred, name in zip(prediction, label_names)}
+print(prediction)
