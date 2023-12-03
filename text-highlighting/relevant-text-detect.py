c5d40e34132caa68eb43d43322fee39e9d25ce7d
Duong Tran Le Truong <truongduongtranle@gmail.com>
2023-12-03 20:41:15 +0700

    change from NLTK to spaCy

--- text-highlighting/relevant-text-detect.py
+++ text-highlighting/relevant-text-detect.py
@@ -1,11 +1,8 @@
-# Import the NLTK and gensim libraries
-import nltk
-import gensim
+# Import the spacy library
+import spacy
 
-filename='GoogleNews-vectors-negative300.bin.gz'
-
-# Load a pre-trained word2vec model
-model = gensim.models.KeyedVectors.load_word2vec_format(filename, binary=True)
+# Load the en_core_web_sm model
+nlp = spacy.load("en_core_web_sm")
 
 # Define a paragraph of text
 paragraph = "Natural language processing (NLP) is a specialized field for analysis and generation of human languages. Human languages, rightly called natural language, are highly context-sensitive and often ambiguous in order to produce a distinct meaning. (Remember the joke where the wife asks the husband to \"get a carton of milk and if they have eggs, get six,\" so he gets six cartons of milk because they had eggs.) NLP provides the ability to comprehend natural language input and produce natural language output appropriately."
@@ -15,31 +12,22 @@ print("Please enter a list of keywords separated by commas:")
 # Read the user input and split it by commas
 keywords = input().split(",")
 
-# Define a similarity threshold
-threshold = 0.8
-
-# Tokenize the paragraph into sentences
-sentences = nltk.sent_tokenize(paragraph)
+# Process the paragraph with spacy
+doc = nlp(paragraph)
 
 # Loop through the sentences and check if they contain any of the keywords or similar words
-for sentence in sentences:
-    # Convert the sentence to lower case
-    sentence = sentence.lower()
-    # Tokenize the sentence into words
-    words = nltk.word_tokenize(sentence)
+for sent in doc.sents:
     # Loop through the keywords
     for keyword in keywords:
         # Strip any whitespace from the keyword
         keyword = keyword.strip()
+        # Process the keyword with spacy
+        keyword_doc = nlp(keyword)
         # Loop through the words in the sentence
-        for word in words:
-            # Check if the word and the keyword are in the model's vocabulary
-            if word in model and keyword in model:
-                # Calculate the cosine similarity between the word and the keyword
-                similarity = model.similarity(word, keyword)
-                # Check if the similarity is above the threshold
-                if similarity > threshold:
-                    # Print the sentence
-                    print(sentence)
-                    # Break the inner loops
-                    break
+        for word in sent:
+            # Check if the word and the keyword have the same lemma (base form)
+            if word.lemma_ == keyword_doc[0].lemma_:
+                # Print the sentence
+                print(sent.text)
+                # Break the inner loops
+                break
