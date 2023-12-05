35b58592ff63bdc86ec79f09700b1ecf99cf9125
Alakinnn <117524098+Alakinnn@users.noreply.github.com>
2023-12-05 20:54:34 +0700

    upload more options

--- mDeberta.py
+++ mDeberta.py
@@ -6,8 +6,8 @@ model_name = "MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
-premise = "The constant demand for productivity has made people more industrious, driving them to enhance time-management skills and adopt innovative technologies."
-hypothesis = "The people of the 21st century are hardworking due to the competitive environment, where success often hinges on one's ability to navigate challenges, adapt to rapid changes, and consistently strive for excellence. The relentless pursuit of goals is fueled by a globalized world that demands continuous innovation and a strong work ethic. In this era, individuals are motivated not only by personal ambition but also by the necessity to stay relevant and contribute meaningfully in a dynamic and ever-evolving professional landscape."
+premise = "The people of this century is as lazy and lax as ever"
+hypothesis = "The people of the 21st century are hardworking due to the competitive environment, where success often hinges on one's ability to navigate challenges, adapt to rapid changes, and consistently strive for excellence."
 
 input = tokenizer(premise, hypothesis, truncation=True, return_tensors="pt")
 output = model(input["input_ids"].to(device))  # device = "cuda:0" or "cpu"
--- robertaAPI.py
+++ robertaAPI.py
@@ -14,9 +14,9 @@ async def query(payload):
 
 
 async def main():
-    premise = "The constant demand for productivity has made people more industrious, driving them to enhance time-management skills and adopt innovative technologies."
+    premise = "The people of this century is as lazy and lax as ever"
 
-    hypothesis = "The people of the 21st century are hardworking due to the competitive environment, where success often hinges on one's ability to navigate challenges, adapt to rapid changes, and consistently strive for excellence. The relentless pursuit of goals is fueled by a globalized world that demands continuous innovation and a strong work ethic. In this era, individuals are motivated not only by personal ambition but also by the necessity to stay relevant and contribute meaningfully in a dynamic and ever-evolving professional landscape."
+    hypothesis = "The people of the 21st century are hardworking due to the competitive environment, where success often hinges on one's ability to navigate challenges, adapt to rapid changes, and consistently strive for excellence."
 
     output = await query({
         "inputs": f"premise: {premise} hypothesis: {hypothesis}",
