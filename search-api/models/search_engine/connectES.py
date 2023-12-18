ca189b46eede13d009011c11771e04d81bea5381
MasterMobo <mastermobo101@gmail.com>
2023-12-18 20:16:31 +0700

    push to dockerhub

--- search-api/models/search_engine/connectES.py
+++ search-api/models/search_engine/connectES.py
@@ -1,11 +1,20 @@
 from elasticsearch import Elasticsearch
 
+import time
+from elasticsearch import Elasticsearch, exceptions
+
 def connect_elasticsearch():
-    _es = Elasticsearch("http://es01:9200")
-
-    if _es.ping():
-        print('Connected to Elasticsearch!')
-    else:
-        print('Could not connect!')
-
+    print('Connecting to Elasticsearch...')
+    _es = None
+    while _es is None:
+        try:
+            _es = Elasticsearch("http://es01:9200")
+            if _es.ping():
+                print('Connected to Elasticsearch!')
+            else:
+                print('Could not connect! Trying again in 5 seconds.')
+                _es = None
+        except exceptions.ConnectionError:
+            print('Elasticsearch connection failed. Retrying in 5 seconds.')
+        time.sleep(5)
     return _es
