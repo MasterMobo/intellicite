6e58439ffaf78643c23c0b9236e52d5af9a81b05
Alakinnn <117524098+Alakinnn@users.noreply.github.com>
2023-12-20 17:02:36 +0700

    primitive flask app

--- search-api/main.py
+++ search-api/main.py
@@ -1,12 +1,17 @@
-from models.intellicite import IntelliCite
+# from models.intellicite import IntelliCite
+from flask import Flask
+# intellicite = IntelliCite()
 
-intellicite = IntelliCite()
+from routes.user import user_route
+app = Flask(__name__)
+app.register_blueprint(user_route, url_prefix="/api/v1")
 
-result = intellicite.process("sleep deprivation and its effects on human memory")
+# result = intellicite.process("sleep deprivation and its effects on human memory")
 
-for index, paper in enumerate(result):
-    print(f"RESULT {index+1}:")
-    print(f"ABSTRACT: {paper['abstract'][:200]}")
-    print(f"HIGHLIGHTS: {paper['highlights']}")
-    print(f"SENTIMENT: {paper['sentiment']}")
-    print("--------------------------------------------------")
+# for index, paper in enumerate(result):
+#     print(f"RESULT {index+1}:")
+#     print(f"ABSTRACT: {paper['abstract'][:200]}")
+#     print(f"HIGHLIGHTS: {paper['highlights']}")
+#     print(f"SENTIMENT: {paper['sentiment']}")
+#     print("--------------------------------------------------")
+
---
+++ search-api/routes/user.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, render_template, request, jsonify
+from models.intellicite import IntelliCite
+intellicite = IntelliCite()
+
+user_route = Blueprint("user_route", __name__)
+
+# Basic routes
+
+# @user_route.route("/")
+# def homepage():
+#   return render_template("home.html")
+
+# @user_route.route("/login")
+# def homepage():
+#   return render_template("login.html")
+
+# @user_route.route("/")
+# def homepage():
+#   return render_template("register.html")
+
+# user input routes　(POST)
+
+@user_route.route("/process", methods=["POST"])
+def process():
+    user_input = request.json.get("user_input")
+    if not user_input:
+        return jsonify({
+            "error": "Missing input"
+        }), 400
+    
+    papers = intellicite.process(user_input)
+
+    response = []
+    for index, paper in enumerate(papers):
+        response.append({
+            "paper_index": index + 1,
+            "abstract": paper["abstract"][:200],
+            "highlights": paper["highlights"],
+            "sentiment": paper["sentiment"] 
+        })
