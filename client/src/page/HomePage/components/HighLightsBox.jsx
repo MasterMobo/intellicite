d54451ea763853b5c398a6b48fc5c83fec34c2d9
MasterMobo <mastermobo101@gmail.com>
2023-12-29 17:32:42 +0700

    changed search interface

---
+++ client/src/page/HomePage/components/HighLightsBox.jsx
@@ -0,0 +1,43 @@
+import { Box, Typography } from "@mui/material";
+
+function HighlightsBox({ highlights }) {
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "row",
+                flexWrap: "wrap",
+                gap: 1,
+                bgcolor: "rgba(0, 0, 0, 0.1)",
+                p: 1,
+                borderRadius: 1,
+            }}
+        >
+            {highlights.map((highlight, index) => {
+                if (index === 0) {
+                    return (
+                        <Typography variant="body2" key={index}>
+                            &quot;{highlight}...
+                        </Typography>
+                    );
+                }
+
+                if (index === highlights.length - 1) {
+                    return (
+                        <Typography variant="body2" key={index}>
+                            {highlight}...&quot;
+                        </Typography>
+                    );
+                }
+
+                return (
+                    <Typography variant="body2" key={index}>
+                        ...{highlight}...
+                    </Typography>
+                );
+            })}
+        </Box>
+    );
+}
+
+export default HighlightsBox;
---
+++ client/src/page/HomePage/components/PaperSearchResult.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Paper, Divider, Stack, Collapse } from "@mui/material";
+import HighlightsSection from "./HighlightsSection";
+import PaperDescription from "./PaperDescription";
+import PaperHeader from "./PaperHeader";
+
+function PaperSearchResult({ paper, index }) {
+    const [expanded, setExpanded] = useState(true);
+
+    return (
+        <Paper key={index} sx={{ p: 2 }}>
+            <PaperHeader
+                paper={paper}
+                expanded={expanded}
+                setExpanded={setExpanded}
+            ></PaperHeader>
+            <Divider />
+
+            <Stack spacing={1}>
+                <PaperDescription paper={paper}></PaperDescription>
+
+                <Collapse in={expanded}>
+                    <HighlightsSection
+                        highlights={paper.highlights}
+                    ></HighlightsSection>
+                </Collapse>
+            </Stack>
+        </Paper>
+    );
+}
+
+export default PaperSearchResult;
