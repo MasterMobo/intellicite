5109cdcd6728f7f1afc8488a7c0aa62dbeeb153f
MasterMobo <mastermobo101@gmail.com>
2023-12-29 15:34:23 +0700

    updated file structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import MenuProvider from './layouts/MenuProvider'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import MenuProvider from "./layouts/MenuProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MenuProvider>
-    <App />
-    </MenuProvider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <MenuProvider>
+            <App />
+        </MenuProvider>
+    </React.StrictMode>
+);
