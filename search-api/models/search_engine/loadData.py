cb3f74f3a7125df9f451cdaf2939af940ae75e46
mh12122003 <96429141+mh12122003@users.noreply.github.com>
2024-01-01 20:54:38 +0700

    Update loadData.py

--- search-api/models/search_engine/loadData.py
+++ search-api/models/search_engine/loadData.py
@@ -2,35 +2,26 @@ import os
 import json
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-def get_papers_json():
+def get_papers_json(dataset, file):
     os.environ['KAGGLE_USERNAME'] = "buidangkhoa"
     os.environ['KAGGLE_KEY'] = "495bdf9d087690b2212b0edfc6c10acc"
-    # Replace path with the actual path of the API key
+    # Replace with path of the API key on local
     os.environ["KAGGLE_CONFIG_DIR"] = "C:/Users/Admin/kaggle/kaggle.json"
-
-    filename = 'arxiv-metadata-oai-snapshot.json'
-
-    # Get path to current directory
-    path = os.path.dirname(os.path.realpath(__file__))
 
     api = KaggleApi()
     api.authenticate()
 
-    dataset_path = os.path.join(path, 'data')
-    os.makedirs(dataset_path, exist_ok=True)
+    os.makedirs(dataset, exist_ok=True)
 
     # Check if dataset is already downloaded
-    if not os.path.exists(os.path.join(dataset_path, filename)):
+    if not os.path.exists(os.path.join(dataset, file)):
         print("Downloading dataset...")
-        api.dataset_download_files('Cornell-University/arxiv', path=dataset_path, unzip=True)
+        api.dataset_download_files('Cornell-University/arxiv', path=dataset, unzip=True)
         print("Dataset downloaded successfully")
 
     # Get the path of the data.json file
-    final_path = os.path.join(dataset_path, filename)
+    final_path = os.path.join(dataset, file)
 
     with open(final_path, 'r') as file:
         for line in file:
             yield json.loads(line)
-
-    # Delete the file after reading
-    os.remove(final_path)
