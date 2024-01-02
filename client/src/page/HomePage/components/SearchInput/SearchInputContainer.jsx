d564c340d5f73ce74523a073c323927df267b701
MasterMobo <mastermobo101@gmail.com>
2024-01-02 19:30:56 +0700

    updated resizable

--- client/src/page/HomePage/components/SearchInput/SearchInputContainer.jsx
+++ client/src/page/HomePage/components/SearchInput/SearchInputContainer.jsx
@@ -3,6 +3,7 @@ import {
     Stack,
     TextareaAutosize,
     CircularProgress,
+    Box,
 } from "@mui/material";
 import useResponsive from "../../hooks/useResponsive";
 import { Resizable } from "re-resizable";
@@ -13,42 +14,58 @@ function SearchInputContainer({ handleSearch, searchState, setSearchText }) {
     return (
         <Resizable
             defaultSize={{
-                width: 320,
-                height: 200,
+                width: "50%",
             }}
             maxWidth={isTablet ? "100%" : "70%"}
             minWidth={isTablet ? "100%" : "20%"}
             enable={isTablet ? {} : { right: true }}
+            handleStyles={{ right: { width: "5px", right: 0 } }}
         >
             <Stack
-                direction="column"
-                spacing={2}
-                width={"100%"}
-                maxHeight={"100%"}
-                pr={2}
-                borderRight={"1px solid black"}
+                direction={"row"}
+                gap={isTablet ? 0 : 3}
+                sx={{ height: "100%" }}
             >
-                <TextareaAutosize
-                    aria-label="minimum height"
-                    minRows={10}
-                    placeholder="Place your query here"
-                    onChange={(e) => setSearchText(e.target.value)}
-                />
-                <Button
-                    variant="contained"
-                    sx={{ height: "2.2rem" }}
-                    onClick={handleSearch}
+                <Stack
+                    direction="column"
+                    spacing={2}
+                    width={"100%"}
+                    maxHeight={"100%"}
                 >
-                    {searchState === "loading" && (
-                        <CircularProgress
-                            size={20}
-                            sx={{
-                                color: "black",
-                            }}
-                        />
-                    )}
-                    {searchState !== "loading" && "Find Sources"}
-                </Button>
+                    <TextareaAutosize
+                        aria-label="minimum height"
+                        minRows={10}
+                        placeholder="Place your query here"
+                        onChange={(e) => setSearchText(e.target.value)}
+                    />
+                    <Button
+                        variant="contained"
+                        sx={{ height: "2.2rem" }}
+                        onClick={handleSearch}
+                    >
+                        {searchState === "loading" && (
+                            <CircularProgress
+                                size={20}
+                                sx={{
+                                    color: "black",
+                                }}
+                            />
+                        )}
+                        {searchState !== "loading" && "Find Sources"}
+                    </Button>
+                </Stack>
+
+                {/* Resizable sidebar */}
+                <Box
+                    sx={{
+                        p: 0,
+                        m: 0,
+                        bgcolor: "rgba(0, 0, 0, 0.1)",
+                        width: "2px",
+                        height: "100%",
+                        display: isTablet ? "none" : "block",
+                    }}
+                ></Box>
             </Stack>
         </Resizable>
     );
