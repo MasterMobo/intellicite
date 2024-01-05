b51d8ac462f453f5ad7044c8829062827cb4bb2f
Duong Tran Le Truong <truongduongtranle@gmail.com>
2024-01-05 16:34:34 +0700

    fix navbar, add copyright, add popup
    somehow I can't import the popup, if import is good then everything should be like this: enter query -> hit search button -> popup appears on top of search results
    
    then can click outside of the popup to make it disappear
    
    IF ONLY VS CODE LET ME IMPORT THE FKN THING

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import MenuProvider from "./layouts/MenuProvider";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import MenuProvider from './layouts/MenuProvider'
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <MenuProvider>
-            <App />
-        </MenuProvider>
-    </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MenuProvider>
+      <App />
+      <ToastContainer />
+    </MenuProvider>
+  </React.StrictMode>,
+)
