958a50b740d887ea0e670dba5c4bff68fc9779bd
Duong Tran Le Truong <truongduongtranle@gmail.com>
2024-01-06 10:10:11 +0700

    fix navbar, add header
    -navbar now has background color, bullet points become button that will change color when hover, now can display actual username instead of (username), has a field for input email when sign up
    -add a header with 2 styles like Sourcely, upper is orange and lower is black, both with white text
    -fix the fonts to Noto Sans

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,17 +1,19 @@
 import NavBar from "./NavBar";
 import MenuContext from "./Context";
 import { useContext } from "react";
+import Header from "./Header";
 
 function MainLayout({ children }) {
     const { isOpenMenu, toggleMenu  } = useContext(MenuContext);
     return ( 
         <div className="row w-full justify-between" style={{ margin: 0 }}>
-            <div className={isOpenMenu ? 'col-9' : 'col-12'}>
+            <Header/>
+            <div style={{ marginTop: '50px' }} className={isOpenMenu ? 'col-9' : 'col-12'}>
             <div className="main">
             {children}
             </div>
             </div>
-            <div className={isOpenMenu ? 'col-3' : ''}>
+            <div style={{ marginTop: '10px' }} className={isOpenMenu ? 'col-3' : ''}>
                 {
                     isOpenMenu ? (
                         <NavBar isOpenMenu={isOpenMenu} toggleMenu={toggleMenu} />
@@ -25,7 +27,7 @@ function MainLayout({ children }) {
                }
             </div>
             <div className="footer">
-                <p>&copy; {new Date().getFullYear()} Your Company Name. All rights reserved.</p>
+                <p>&copy; {new Date().getFullYear()} Intellicite. All rights reserved.</p>
             </div>
         </div>
      );
